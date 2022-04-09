c6b2bc01a0d6b8400be909851c3f5cb59673fa46
jenochjones <jon.enoch.jones@gmail.com>
2022-04-09 08:39:31 -0600

    Updates

---
+++ precache-manifest.be238cd91f8837ed0ab7ff3a27356e2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "24166e93e386b9d82269",
+    "url": "/jenochjones/minesweeper/static/css/main.86dca0fd.chunk.css"
+  },
+  {
+    "revision": "24166e93e386b9d82269",
+    "url": "/jenochjones/minesweeper/static/js/main.24166e93.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/jenochjones/minesweeper/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "73c0e6f4e23f79a357e5",
+    "url": "/jenochjones/minesweeper/static/js/2.73c0e6f4.chunk.js"
+  },
+  {
+    "revision": "72593c8320cf6b2b7a9f",
+    "url": "/jenochjones/minesweeper/static/js/runtime~main.72593c83.js"
+  },
+  {
+    "revision": "90a693454b21d6eed9b4c25f80a29074",
+    "url": "/jenochjones/minesweeper/static/media/bomb.90a69345.png"
+  },
+  {
+    "revision": "a821390a44cf55309ab4e15a1a48d48e",
+    "url": "/jenochjones/minesweeper/static/media/flag.a821390a.png"
+  },
+  {
+    "revision": "a4160711ad51ff12e09912932c403a1e",
+    "url": "/jenochjones/minesweeper/index.html"
+  }
+];
